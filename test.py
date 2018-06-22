33f1f7695eca77c91db263c2e005d1962b58bed3
LucasLermen <lucas.arthur.lermen@gmail.com>
2018-06-22 18:13:54 -0300

    Adicionando testes para print_tree

--- test.py
+++ test.py
@@ -1,21 +1,52 @@
 import unittest
+import StringIO
 import os
 import subprocess
 import re
+import sys
 from diskspace import diskspace
 
 class DiskspaceTest(unittest.TestCase):
 
     def setUp(self):
-        self.command = 'ls'
+        self.path = '/home/TecProg/Test'
+        self.total_size = 6
+        self.file_tree_node = {'print_size': '100.00Kb',
+                               'children': [], 'size': self.total_size}
+        self.file_tree = {self.path: self.file_tree_node}
+        self.largest_size = 8
 
     def testOutputForCommand(self):
-        checkReturn = subprocess.check_output(self.command)
-        self.assertEqual(diskspace.subprocess_check_output(self.command), checkReturn)
+        command = 'ls'
+        checkReturn = subprocess.check_output(command)
+        self.assertEqual(diskspace.subprocess_check_output(command), checkReturn)
 
     def testBytesToReadable(self):
-        blocksBytes= 1
-        self.assertEqual(diskspace.bytes_to_readable(blocksBytes), '512.00B')
+        blocksBytes = 0
+        readable = diskspace.bytes_to_readable(blocksBytes)
+        self.assertEqual(readable, '0.00B')
+
+        blocksBytes = 1
+        readable = diskspace.bytes_to_readable(blocksBytes)
+        self.assertEqual(readable, '512.00B')
+
+        blocksBytes = 200
+        readable = diskspace.bytes_to_readable(blocksBytes)
+        self.assertEqual(readable, '100.00Kb')
+
+        blocksBytes = 491510
+        readable = diskspace.bytes_to_readable(blocksBytes)
+        self.assertEqual(readable, '240.00Mb')
+
+    def testPrintTree(self):
+        captured = StringIO.StringIO()
+        sys.stdout = captured
+
+        diskspace.print_tree(self.file_tree, self.file_tree_node, self.path,
+                             self.largest_size, self.total_size)
+        result = "100.00Kb  100%  {}\n".format(self.path)
+        sys.stdout = sys.__stdout__
+        self.assertEqual(result, captured.getvalue())
 
 
 

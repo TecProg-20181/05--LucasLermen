dba1f8369b487b92d7cf0e2491f49b1848833c05
LucasLermen <lucas.arthur.lermen@gmail.com>
2018-06-22 15:13:50 -0300

    Testando subprocess e BytesToReadable.

--- test.py
+++ test.py
@@ -1,13 +1,23 @@
 import unittest
+import os
+import subprocess
+import re
+from diskspace import diskspace
 
-#from diskspace.diskspace import diskspace
+class DiskspaceTest(unittest.TestCase):
 
-class SubprocessCheckOutputTest(unittest.TestCase):
+    def setUp(self):
+        self.command = 'ls'
 
     def testOutputForCommand(self):
-        command = 'output test'
-        output = diskspace.subprocess_check_output(command)
-        self.assertEqual('test\n', output)
+        checkReturn = subprocess.check_output(self.command)
+        self.assertEqual(diskspace.subprocess_check_output(self.command), checkReturn)
 
-suite = unittest.TestLoader().loadTestsFromTestCase(SubprocessCheckOutputTest)
-unittest.TextTestRunner(verbosity=3).run(suite)
+    def testBytesToReadable(self):
+        blocksBytes= 1
+        self.assertEqual(diskspace.bytes_to_readable(blocksBytes), '512.00B')
+
+
+
+if __name__ == '__main__':
+    unittest.main()
